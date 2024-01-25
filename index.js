3048225b033f3dfb95b6d447699554c057427888
Iida Lassila <iida.m.lassila@helsinki.fi>
2024-01-25 15:00:39 +0200

    Exercises 3.1 - 3.6

--- index.js
+++ index.js
@@ -30,6 +30,58 @@ app.get('/api/persons', (request, response) => {
 response.json(persons)
 })
 
+app.get('/info', (request, response) => {
+    const length = persons.length
+    const date = new Date()
+    response.send(`<p>Phonebook has info for ${length} persons</p>
+    <p>${date}</p>`)
+})
+
+app.get('/api/persons/:id', (request, response) => {
+    const id = request.params.id
+    const person = persons.find(person => person.id === id)
+  
+    if (person) {
+      response.json(person)
+    } else {
+      response.status(404).end()
+    }
+  })
+
+app.delete('/api/persons/:id', (request, response) => {
+    const id = request.params.id
+    persons = persons.filter(person => person.id !== id)
+
+    response.status(204).end()
+})
+
+app.post('/api/persons', (request, response) => {
+    const body = request.body
+
+    if (!body.name || !body.number) {
+        return response.status(400).json({ 
+          error: 'content missing' 
+        })
+      }
+
+      if (persons.some(person => person.name === body.name)) {
+        return response.status(400).json({ 
+          error: 'name must be unique' 
+        });
+    }
+
+    const newId = Math.floor(Math.random() * 100000)
+
+    const person = {
+        name: body.name,
+        number: body.number,
+        id: newId.toString(),
+      }
+    
+    persons = persons.concat(person)
+    response.json(person)
+  })
+
 const PORT = 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
