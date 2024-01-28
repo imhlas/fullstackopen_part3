ea3ca43c4729dfdea0ff126ada382f2d9d4c7f14
Iida Lassila <iida.m.lassila@helsinki.fi>
2024-01-28 14:57:40 +0200

    Exercises 3.19 - 3.20

--- index.js
+++ index.js
@@ -52,12 +52,6 @@ app.delete('/api/persons/:id', (request, response, next) => {
 app.post('/api/persons', (request, response, next) => {
   const body = request.body
 
-  if (!body.name || !body.number) {
-      return response.status(400).json({ 
-          error: 'content missing' 
-      })
-  }
-
   Person.findOne({ name: body.name })
       .then(foundPerson => {
           if (foundPerson) {
@@ -88,7 +82,8 @@ app.put('/api/persons/:id', (request, response, next) => {
       number: body.number,
   }
 
-  Person.findByIdAndUpdate(request.params.id, person, { new: true })
+  Person.findByIdAndUpdate(request.params.id, person,
+    { new: true, runValidators: true, context: 'query'})
     .then(updatedPerson => {
       response.json(updatedPerson)
     })
@@ -106,6 +101,8 @@ const errorHandler = (error, request, response, next) => {
 
   if (error.name === 'CastError') {
     return response.status(400).send({ error: 'malformatted id' })
+  } else if(error.name === 'ValidationError') {
+      return response.status(400).json({ error: error.message })
   }
 
   next(error)
