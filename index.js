fd1ca1b11ac77b0ee0d1d89b47c1c2bce7c53b60
Iida Lassila <iida.m.lassila@helsinki.fi>
2024-01-28 12:52:22 +0200

    3.15 - 3.18

--- index.js
+++ index.js
@@ -19,53 +19,101 @@ app.get('/api/persons', (request, response) => {
   })
 })
 
-app.get('/info', (request, response) => {
-    const length = persons.length
-    const date = new Date()
-    response.send(`<p>Phonebook has info for ${length} persons</p>
-    <p>${date}</p>`)
+app.get('/info', (request, response, next) => {
+    Person.countDocuments()
+    .then(length => {
+        const date = new Date()
+        response.send(`<p>Phonebook has info for ${length} persons</p>
+                      <p>${date}</p>`)
+})
+.catch(error => next(error))
 })
 
-app.get('/api/persons/:id', (request, response) => {
-    Person.findById(request.params.id).then(person => {
+app.get('/api/persons/:id', (request, response, next) => {
+    Person.findById(request.params.id)
+    .then(person => {
+        if (person) {
         response.json(person)
-    })
+    } else {
+        response.status(404).end()
+    }
+  })
+  .catch(error => next(error))
 })
 
-app.delete('/api/persons/:id', (request, response) => {
+app.delete('/api/persons/:id', (request, response, next) => {
   Person.findByIdAndDelete(request.params.id)
     .then(result => {
-      if (result) {
-        response.status(204).end()
-      } else {
-        response.status(404).json({ error: 'person not found' })
-      }
+      response.status(204).end()
     })
-  })
-
-app.post('/api/persons', (request, response) => {
-    const body = request.body
-
-    if (!body.name || !body.number) {
-        return response.status(400).json({ 
-          error: 'content missing' 
-        })
-      }
-
-    const newId = Math.floor(Math.random() * 100000)
-
-    const person = new Person({
-        name: body.name,
-        number: body.number,
-        id: newId.toString(),
-      })
-
-    person.save().then(savedPerson => {
-        response.json(savedPerson)
-      })
+    .catch(error => next(error))
 })
+
+app.post('/api/persons', (request, response, next) => {
+  const body = request.body
+
+  if (!body.name || !body.number) {
+      return response.status(400).json({ 
+          error: 'content missing' 
+      })
+  }
+
+  Person.findOne({ name: body.name })
+      .then(foundPerson => {
+          if (foundPerson) {
+              response.status(400).json({
+                  error: 'name must be unique'
+              });
+          } else {
+              const person = new Person({
+                  name: body.name,
+                  number: body.number
+              });
+
+              person.save()
+                  .then(savedPerson => {
+                      response.json(savedPerson)
+                  })
+                  .catch(error => next(error))
+          }
+      })
+      .catch(error => next(error))
+})
+
+app.put('/api/persons/:id', (request, response, next) => {
+  const body = request.body
+
+  const person = {
+      name: body.name,
+      number: body.number,
+  }
+
+  Person.findByIdAndUpdate(request.params.id, person, { new: true })
+    .then(updatedPerson => {
+      response.json(updatedPerson)
+    })
+    .catch(error => next(error))
+})
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
     
-  const PORT = process.env.PORT
-  app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
-  })
+app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  }
+
+  next(error)
+}
+
+app.use(errorHandler)
+
+const PORT = process.env.PORT
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
